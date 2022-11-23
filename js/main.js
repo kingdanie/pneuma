1f474d2c8944d0b97916bc0f2a606c2e13fe9f89
kingdanie <daniedemola@gmail.com>
2022-11-23 23:36:10 +0100

    add expiry date

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("March 25, 2021 15:37:25").getTime();
+        var finalDate =  new Date("December 10, 2022 23:59:25").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
