ba51fd91cb041463c543db392e8debf4b12ab9bf
kingdanie <daniedemola@gmail.com>
2022-12-07 10:39:19 +0100

    nav menu links

--- pneumaoutsourcing/components/NavBar.tsx
+++ pneumaoutsourcing/components/NavBar.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export default function NavBar() {
+
+    const appLinks = [
+        {name: 'about', url: '/about'},
+        {name: 'services', url: '/services'},
+        {name: 'contact', url: '/contact'},
+    ]
     return (
-        <div className="" style={{display: "flex", justifyContent: "space-around"}}>
-            <div>logo</div>
-            <div>NavBar</div>
+        <div className="" 
+            style={{display: "flex", justifyContent: "space-between", padding: 25}}>
+            <Link href={'/'}>
+            <Image src='/Pneuma1.png' alt='logo' width='150' height='45'/>
+            </Link>
+            {/* <div>NavBar</div> */}
+            <nav style={{display: "flex", gap: 25,}}>
+                {appLinks.map(navLink => (
+                    <Link key={navLink.name} href={navLink.url}>
+                            {navLink.name.toUpperCase()}
+                    </Link>
+                ))}
+            </nav>
         </div>
     )
 }
