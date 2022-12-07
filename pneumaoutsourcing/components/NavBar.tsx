ebb584fc8a24298f96d806d5dab6bac85d6cec00
kingdanie <daniedemola@gmail.com>
2022-12-07 09:21:49 +0100

    nav init

---
+++ pneumaoutsourcing/components/NavBar.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <div className="" style={{display: "flex", justifyContent: "space-around"}}>
+            <div>logo</div>
+            <div>NavBar</div>
+        </div>
+    )
+}
