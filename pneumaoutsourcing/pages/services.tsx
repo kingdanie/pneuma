e4fbd42da47cbb33ed18e69fc0c774cb04903bd5
kingdanie <daniedemola@gmail.com>
2022-12-07 23:51:53 +0100

    services title

--- pneumaoutsourcing/pages/services.tsx
+++ pneumaoutsourcing/pages/services.tsx
@@ -1,8 +1,12 @@
+import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
 
 export default function Services() {
     return (
         <div>
-            <h1>About Us</h1>
+            <section>
+            <h1>Our Services</h1>
+            </section>
         </div>
     )
 }
