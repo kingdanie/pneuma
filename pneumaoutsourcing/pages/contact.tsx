f1153860e4de08e69c9ecd4ded9c6ecb251ce466
kingdanie <daniedemola@gmail.com>
2022-12-10 00:56:43 +0100

    add seo header to contact page

--- pneumaoutsourcing/pages/contact.tsx
+++ pneumaoutsourcing/pages/contact.tsx
@@ -1,67 +1,70 @@
+import Head from "next/head";
 
 export default function About() {
-    return (
-        <>
-   <section
+  return (
+    <>
+      <Head>
+        <title>Contact Us</title>
+        <meta
+          name="description"
+          content="Pneuma Outsourcing is commited to acquiring 
+            and managing the most human 
+            resource for businesses to thrive
+          "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section
+        className="p-flex  Width-1040"
         style={{
-          display: "flex",
-          alignItems: "center",
-          background: "#efefef",
           padding: 50,
-          justifyContent: 'center'
         }}
       >
-
         <h1>Contact Us</h1>
       </section>
       <section
         style={{
           display: "flex",
           alignItems: "center",
-          flex: '1 1 0px',
-          padding: '50px 150px',
-          gap: 10
+          flex: "1 1 0px",
+          padding: "50px 150px",
+          gap: 10,
         }}
       >
-        <div style={{width: '100%'}}>
-        <h1>Contact Us</h1>
-          <p>We look forward to discussing a new project with you. 
-            For inquiries or quotes, contact us via:
-        </p>
-        <div>
-            Adress
+        <div style={{ width: "100%" }}>
+          <h1>Contact Us</h1>
+          <p>
+            We look forward to discussing a new project with
+            you. For inquiries or quotes, contact us via:
+          </p>
+          <div>Adress</div>
+          <div>Phone</div>
+          <div>Social Media</div>
         </div>
-        <div>
-            Phone
-        </div>
-        <div>
-            Social Media
-        </div>
-        
-        </div>
-        <div style={{width: '100%'}}>
-            <form style={{background: '#efefef', padding: 25}}>
-                <div>
-
-                <input type="text" placeholder="your name" />
-                </div>
-                <div>
-
-<input type="text" placeholder="your email" />
-</div>
+        <div style={{ width: "100%" }}>
+          <form
+            style={{ background: "#efefef", padding: 25 }}
+          >
             <div>
-
-                <textarea name="mesage" id="" cols="30" rows="10"></textarea>
+              <input type="text" placeholder="your name" />
             </div>
             <div>
-                <button>
-                    Submit
-                </button>
+              <input type="text" placeholder="your email" />
             </div>
-            </form>
+            <div>
+              <textarea
+                name="mesage"
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+            <div>
+              <button>Submit</button>
+            </div>
+          </form>
         </div>
-
       </section>
-        </>
-    )
+    </>
+  );
 }
