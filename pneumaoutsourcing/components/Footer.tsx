fd739d4ce89313d3c5a8bf293c49ee22964e1d38
kingdanie <daniedemola@gmail.com>
2022-12-10 00:55:49 +0100

    centralize text in footer

--- pneumaoutsourcing/components/Footer.tsx
+++ pneumaoutsourcing/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div style={{padding: 20}}>
+        <div style={{padding: 20, textAlign: 'center', fontSize: '0.8em'}}>
             <span>© Pneuma Outsourcing {new Date().getFullYear()}. All Rights Reserved.</span>
         </div>
     )
