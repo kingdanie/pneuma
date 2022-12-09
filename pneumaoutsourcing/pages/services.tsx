ac1023249384598ac7c9637e2d50e852136157c9
kingdanie <daniedemola@gmail.com>
2022-12-10 00:56:23 +0100

    add some content to services page

--- pneumaoutsourcing/pages/services.tsx
+++ pneumaoutsourcing/pages/services.tsx
@@ -1,12 +1,22 @@
-import Footer from "../components/Footer"
-import NavBar from "../components/NavBar"
+import Head from "next/head"
+import { ContactCard } from "../components/ContactCard"
+
 
 export default function Services() {
     return (
         <div>
-            <section>
+        <Head>
+        <title>Our Services</title>
+        <meta
+          name="description"
+          content="Pneuma Outsourcing is commited to acquiring and managing the most human resource for businesses to thrive"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+            <section className="p-flex  Width-1040">
             <h1>Our Services</h1>
             </section>
+            <ContactCard />
         </div>
     )
 }
