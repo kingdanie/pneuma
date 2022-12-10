fbbb06bb29faa7702dbc367dc1ff2a4e4ea6e24d
kingdanie <daniedemola@gmail.com>
2022-12-10 23:55:13 +0100

    404 page complete

--- pneumaoutsourcing/pages/404.tsx
+++ pneumaoutsourcing/pages/404.tsx
@@ -1,8 +1,14 @@
+import { ContactCard } from "../components/ContactCard";
 
 export default function Page404() {
         return (
             <div>
-                <h1>Page not found</h1>
+                <section className="Width-1040" style={{textAlign: 'center'}}>
+                                    <h1 className="colored-text">Page or Resource not found</h1>
+                <p>Please reach out to us for your inquiries or questions</p>
+                </section>
+
+                <ContactCard />
             </div>
         )
 }
