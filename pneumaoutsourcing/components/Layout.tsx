4d7565917c3fbbbba6391b3867cc4dfe7a3cab72
kingdanie <daniedemola@gmail.com>
2022-12-10 21:19:27 +0100

    add mobile menu to layout

--- pneumaoutsourcing/components/Layout.tsx
+++ pneumaoutsourcing/components/Layout.tsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import Footer from "./Footer";
+import { MenuIcon } from "./MenuIcon";
+import { MobileMenu } from "./MobileMenu";
 import NavBar from "./NavBar";
 
-export  const Layout = ({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) => {
+export  const Layout = ({ children }: { children: React.ReactNode }) => {
+
+    const [menuState, setMenuState] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuState(!menuState)
+    }
     return (
-        <>
-        <NavBar />
+        <div className={`${menuState ? 'info-is-visible' : ''}`}>
+        {/* <MenuIcon menuState={menuState} toggleMenu={toggleMenu} />  */}
+                <NavBar menuState={menuState} toggleMenu={toggleMenu} />
+
         <main style={{minHeight: '70vh'}}>
           {/* Primary column */}
           {children}
         </main>
         <Footer />
-        </>
+        <MobileMenu />
+        </div>
     )
 }
