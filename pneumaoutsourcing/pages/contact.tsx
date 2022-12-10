a2ae426a97b0e1d904505d0a33558b3d0ebcd3e7
kingdanie <daniedemola@gmail.com>
2022-12-10 23:56:21 +0100

    add content to about page

--- pneumaoutsourcing/pages/contact.tsx
+++ pneumaoutsourcing/pages/contact.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -15,28 +16,36 @@ export default function About() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section
-        className="p-flex  Width-1040"
+        className="Width-1040"
         style={{
           padding: 50,
         }}
       >
-        <h1>Contact Us</h1>
+        <h1>Reach Out To Us</h1>
+        <p>
+          Contact us today to learn more about our services
+          and how we can help your business achieve its
+          goals. Our team of experienced professionals is
+          ready to assist you with all of your business
+          needs. Whether you're looking for advice on
+          strategic planning, help with market analysis, or
+          support with organizational development, we're
+          here to help. We offer a range of <Link href={'/services'}>services</Link> and
+          will work with you to develop a customized plan
+          that meets your unique needs. Contact us now to
+          schedule a consultation and take the first step
+          towards improving your business.
+        </p>
       </section>
-      <section
+      <section className="Width-1040 p-flex"
         style={{
-          display: "flex",
           alignItems: "center",
           flex: "1 1 0px",
-          padding: "50px 150px",
           gap: 10,
         }}
       >
         <div style={{ width: "100%" }}>
-          <h1>Contact Us</h1>
-          <p>
-            We look forward to discussing a new project with
-            you. For inquiries or quotes, contact us via:
-          </p>
+      
           <div>Adress</div>
           <div>Phone</div>
           <div>Social Media</div>
