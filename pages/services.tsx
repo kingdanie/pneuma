0a8ba1d1a198a33cc20cee2902ca929572609c00
kingdanie <daniedemola@gmail.com>
2022-12-16 18:36:15 +0100

    add calendly link to services page

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import { ContactCard } from "../components/ContactCard";
 
 export default function Services() {
@@ -14,10 +15,12 @@ export default function Services() {
       </Head>
       <section className="p-flex  Width-1040">
         <div>
-                  <h1 className="subtitle">Our Services</h1>
-        <h2>We have strategically positioned our services to tackle the 
-          most challenging touch points currently facing businesses
-        </h2>
+          <h1 className="subtitle">Our Services</h1>
+          <h2>
+            We have strategically positioned our services to
+            tackle the most challenging touch points
+            currently facing businesses
+          </h2>
         </div>
 
         <div className="service-image">
@@ -36,57 +39,72 @@ export default function Services() {
       </section>
       <section className="Width-1040">
         <section>
-          <h3 className="colored-text">Business Consultancy</h3>
+          <h3 className="colored-text">
+            Business Consultancy
+          </h3>
           <p>
-          business consultancy is a company that provides
-          expert advice and assistance to businesses in
-          order to help them improve their operations and
-          achieve their goals. They can provide a wide range
-          of services, including strategic planning, market
-          analysis, and organizational development. Many
-          businesses turn to consultancies when they need
-          help identifying and addressing specific
-          challenges or opportunities, or when they want to
-          tap into the expertise of experienced
-          professionals. Our business consultancy services
-          are designed to help businesses of all sizes
-          achieve their goals. We offer a range of services,
-          including market research, competitive analysis,
-          and business planning. Our experienced team will
-          work with you to understand your unique business
-          needs and develop a customized consultancy plan
-          that will help you achieve your goals. Whether
-          you&apos;re looking to grow your business, increase
-          profitability, or improve your operations, our
-          business consultancy services can help. Contact us
-          today to learn more and schedule a consultation
+            business consultancy is a company that provides
+            expert advice and assistance to businesses in
+            order to help them improve their operations and
+            achieve their goals. They can provide a wide
+            range of services, including strategic planning,
+            market analysis, and organizational development.
+            Many businesses turn to consultancies when they
+            need help identifying and addressing specific
+            challenges or opportunities, or when they want
+            to tap into the expertise of experienced
+            professionals. Our business consultancy services
+            are designed to help businesses of all sizes
+            achieve their goals. We offer a range of
+            services, including market research, competitive
+            analysis, and business planning. Our experienced
+            team will work with you to understand your
+            unique business needs and develop a customized
+            consultancy plan that will help you achieve your
+            goals. Whether you&apos;re looking to grow your
+            business, increase profitability, or improve
+            your operations, our business consultancy
+            services can help.{" "}
+            <Link
+              href={
+                "https://calendly.com/pneumaoutsourcing/30min"
+              }
+              target="_blank"
+            >
+              Schedule a consultation to get started.
+            </Link>
           </p>
-            
         </section>
 
         <section>
-          <h3 className="colored-text">Talent management</h3>
+          <h3 className="colored-text">
+            Talent management
+          </h3>
           <p>
             HR outsourcing is a cost-effective way for
-          businesses to manage their human resources needs.
-          By partnering with an HR outsourcing company,
-          businesses can access a wide range of HR services
-          without the need to hire in-house staff. This can
-          save time and money, while also providing access
-          to experienced HR professionals who can help
-          businesses manage their HR needs more effectively.
-          Our HR outsourcing services are designed to help
-          businesses of all sizes manage their HR needs. We
-          offer a range of services, including payroll,
-          benefits administration, employee relations, and
-          compliance. Our experienced team will work with
-          you to understand your unique business needs and
-          develop a customized HR outsourcing plan that will
-          help you achieve your goals. Whether you&apos;re
-          looking to streamline your HR processes, improve
-          employee satisfaction, or reduce HR costs, our HR
-          outsourcing services can help. Contact us today to
-          learn more and schedule a consultation.
+            businesses to manage their human resources
+            needs. By partnering with an HR outsourcing
+            company, businesses can access a wide range of
+            HR services without the need to hire in-house
+            staff. This can save time and money, while also
+            providing access to experienced HR professionals
+            who can help businesses manage their HR needs
+            more effectively. Our HR outsourcing services
+            are designed to help businesses of all sizes
+            manage their HR needs. We offer a range of
+            services, including payroll, benefits
+            administration, employee relations, and
+            compliance. Our experienced team will work with
+            you to understand your unique business needs and
+            develop a customized HR outsourcing plan that
+            will help you achieve your goals. Whether
+            you&apos;re looking to streamline your HR
+            processes, improve employee satisfaction, or
+            reduce HR costs, our HR outsourcing services can
+            help.{" "}
+            <Link href={"/contact"} target="_blank">
+              Take action now by contacting us now.
+            </Link>
           </p>
         </section>
 
@@ -94,50 +112,71 @@ export default function Services() {
           <h3 className="colored-text">Hr outsourcing </h3>
           <p>
             HR outsourcing is a cost-effective way for
-          businesses to manage their human resources needs.
-          By partnering with an HR outsourcing company,
-          businesses can access a wide range of HR services
-          without the need to hire in-house staff. This can
-          save time and money, while also providing access
-          to experienced HR professionals who can help
-          businesses manage their HR needs more effectively.
-          Our HR outsourcing services are designed to help
-          businesses of all sizes manage their HR needs. We
-          offer a range of services, including payroll,
-          benefits administration, employee relations, and
-          compliance. Our experienced team will work with
-          you to understand your unique business needs and
-          develop a customized HR outsourcing plan that will
-          help you achieve your goals. Whether you&apos;re
-          looking to streamline your HR processes, improve
-          employee satisfaction, or reduce HR costs, our HR
-          outsourcing services can help. Contact us today to
-          learn more and schedule a consultation.
+            businesses to manage their human resources
+            needs. By partnering with an HR outsourcing
+            company, businesses can access a wide range of
+            HR services without the need to hire in-house
+            staff. This can save time and money, while also
+            providing access to experienced HR professionals
+            who can help businesses manage their HR needs
+            more effectively. Our HR outsourcing services
+            are designed to help businesses of all sizes
+            manage their HR needs. We offer a range of
+            services, including payroll, benefits
+            administration, employee relations, and
+            compliance. Our experienced team will work with
+            you to understand your unique business needs and
+            develop a customized HR outsourcing plan that
+            will help you achieve your goals. Whether
+            you&apos;re looking to streamline your HR
+            processes, improve employee satisfaction, or
+            reduce HR costs, our HR outsourcing services can
+            help. Contact us today to learn more or{" "}
+            <Link
+              href={
+                "https://calendly.com/pneumaoutsourcing/30min"
+              }
+              target="_blank"
+            >
+              Schedule an appointment with one of our
+              consultants
+            </Link>
+            .
           </p>
         </section>
 
         <section>
-          <h3 className="colored-text">Coaching and training</h3>
+          <h3 className="colored-text">
+            Coaching and training
+          </h3>
           <p>
-          Coaching and training your staff is an important
-          part of ensuring their success and the success of
-          your business. By providing your employees with
-          the skills and knowledge they need to succeed, you
-          can improve their performance, increase
-          productivity, and drive your business forward. Our
-          coaching and training services are designed to
-          help businesses of all sizes develop their staff.
-          We offer a range of services, including leadership
-          development, team building, and performance
-          coaching. Our experienced team will work with you
-          to understand your unique business needs and
-          develop a customized coaching and training plan
-          that will help you achieve your goals. Whether
-          you&apos;re looking to improve the skills of your
-          existing staff, train new employees, or develop
-          your leadership team, our coaching and training
-          services can help. Contact us today to learn more
-          and schedule a consultation.
+            Coaching and training your staff is an important
+            part of ensuring their success and the success
+            of your business. By providing your employees
+            with the skills and knowledge they need to
+            succeed, you can improve their performance,
+            increase productivity, and drive your business
+            forward. Our coaching and training services are
+            designed to help businesses of all sizes develop
+            their staff. We offer a range of services,
+            including leadership development, team building,
+            and performance coaching. Our experienced team
+            will work with you to understand your unique
+            business needs and develop a customized coaching
+            and training plan that will help you achieve
+            your goals. Whether you&apos;re looking to
+            improve the skills of your existing staff, train
+            new employees, or develop your leadership team,
+            our coaching and training services can help.
+            Contact us today to learn more or{" "}
+            <Link
+              href={
+                "https://calendly.com/pneumaoutsourcing/30min"
+              }
+              target="_blank"
+            >
+              Book consultation.
+            </Link>
           </p>
         </section>
       </section>
