935e1ca544698d667e7b77282b6e985db99f8986
kingdanie <daniedemola@gmail.com>
2022-12-16 18:45:48 +0100

    feat: add text props on calendly btn

--- components/CalendlyBtn.tsx
+++ components/CalendlyBtn.tsx
@@ -1,12 +1,12 @@
 import { PopupButton } from "react-calendly";
 
-export default function CalendlyBtn() {
+export default function CalendlyBtn({text}: {text: string}) {
   return (
     <div className="calendy-btn">
       <PopupButton
         url="https://calendly.com/pneumaoutsourcing/30min"
         rootElement={document.getElementById("__next")}
-        text="Schedule an appointment!"
+        text={text}
       />
     </div>
   );
